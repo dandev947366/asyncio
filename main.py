9432726c47ba4537ff081271389ab2f26038890a
danofoundation <dan.dev947366@gmail.com>
2024-07-26 19:19:32 +0300

    Update main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello')
+import asyncio
+
+async def async_sleep():
+    print('Before sleep')
+    print('Sleeping...')
+    await asyncio.sleep(5)
+    print('After sleep')
+    
+async def return_hello():
+    return 'Hello'
+    
+async def main():
+    await async_sleep()
+    result = await return_hello()
+    print(result)
+    
+if __name__=="__main__":
+    asyncio.run(main())
