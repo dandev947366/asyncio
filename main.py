3396f14bd047970143c04eb783d7a67e0f729952
danofoundation <dan.dev947366@gmail.com>
2024-07-26 20:07:13 +0300

    Add gather

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import asyncio
-
-async def async_sleep():
-    print('Before sleep')
+import time
+async def async_sleep(n):
+    print(f'Before sleep {n}')
     print('Sleeping...')
     await asyncio.sleep(5)
-    print('After sleep')
+    print(f'After sleep {n}')
     
-async def return_hello():
-    return 'Hello'
+async def print_hello():
+    print('Hello')
     
+  
 async def main():
-    await async_sleep()
-    result = await return_hello()
-    print(result)
+    start = time.time()
+    # task = asyncio.create_task(async_sleep(1)) 
+    # await async_sleep(2)
+    # await task
+    # await print_hello()
+    try:
+        await asyncio.gather(asyncio.wait_for(async_sleep(30), 5), async_sleep(6), print_hello())
+    except asyncio.TimeoutError:
+        print('Encountered timeout error')
+    print('total time: ', time.time() - start)
     
 if __name__=="__main__":
     asyncio.run(main())
